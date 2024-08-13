14432cadc3ec83981fe73310d85a207dcfeef528
michelle-curtis <mcurtis459@gmail.com>
2024-08-13 13:24:19 -0400

    added reference choice and starcat functionality

--- minimal/bl_starcat.py
+++ minimal/bl_starcat.py
@@ -1,67 +1,78 @@
 from flask import (
-    Blueprint, render_template, url_for, request, flash
+    Blueprint, render_template, url_for, request, flash, redirect, send_file, Markup
+
 )
 from .layoutUtils import *
 from .auth import *
+import tarfile
+import pandas as pd
+import numpy as np
+import scanpy as sc
+from starcat import starCAT
 
 
 bp = Blueprint('bl_starcat', __name__, url_prefix='/starcat')
 
-@bp.route('/',methods=('GET', 'POST'))
-#@manage_cookie_policy
+
+@bp.route('/run-starcat',methods=('GET', 'POST'))
 def runstarcat():
     mc = set_menu("starcat")
+    set_session()
     page_title = "This is a title that will end up in the page url"
     page_title_for_id = "This is a title that will end up in the page url"
+
+    print(session.get('selected_ref', 'none'))
+
     if request.method == 'POST':
         file = request.files['file']
         if file:
             file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], file.filename)
+            os.makedirs(current_app.config['UPLOAD_FOLDER'], exist_ok=True)
             file.save(file_path)
-            # Run the processing script on the uploaded file
-            #result = process_data(file_path)
-            #flash(f'Processing complete: {result}')
-            #return redirect(url_for('bl_starcat.runstarcat'))
+
+            if session.get('selected_ref'):
+                # flash("processing_message")
+                process_data(file_path)
+                out_file = os.path.join(current_app.config['UPLOAD_FOLDER'], 'starCAT_output.tar.gz')
+                return send_file(os.path.join(os.getcwd(), out_file), as_attachment=True)
+            
+            else:
+                flash("select_ref_message") 
+
     return render_template('starcat/niceurlsspawn.html', mc=mc, 
-        page_title=page_title, page_title_for_id=page_title_for_id)
+        page_title=page_title, page_title_for_id=page_title_for_id, references=session.get('references'), selected_ref = session.get('selected_ref'))
+
+
+@bp.route('/set-session', methods=['POST'])
+def set_session(selected_ref = None):
+    # Add session variables. Update selected reference only to initialize or if passed to the function.
+    session['references'] = ['', 'TCAT.V1', 'BCAT.V1']
+    if (not session.get('selected_ref')) or (selected_ref!=None):
+        session['selected_ref'] = selected_ref
+    return redirect(url_for('bl_starcat.runstarcat'))
+
+
+@bp.route('/choose-ref', methods=('GET', 'POST'))
+def chooseref():    
+    # Update reference 
+    if request.method == 'POST':
+        reference = request.form.get('ref')
+        set_session(selected_ref = reference)
+        return redirect(url_for('bl_starcat.runstarcat'))
+
 
 def process_data(file_path):
-    """
-    Dummy processing function.
-    Replace this with actual data processing logic.
-    """
-    # Open the file and process it
-    with open(file_path, 'r') as file:
-        data = file.read()
+    # Run starCAT
+    out_dir = os.path.join(current_app.config['UPLOAD_FOLDER'], 'starCAT_output')
+    out_file = os.path.join(current_app.config['UPLOAD_FOLDER'], 'starCAT_output.tar.gz')
+    os.makedirs(out_dir, exist_ok=True)
 
-    # Here you can add the logic to process the data
-    # For example, you can parse CSV files, run computations, etc.
-    # This is a placeholder for demonstration purposes.
-    processed_data = f"Data processed for file: {file_path}"
+    cat = starCAT(reference=session.get('selected_ref'), cachedir=current_app.config['UPLOAD_FOLDER'])
+    adata = cat.load_counts(file_path)
+    usage, scores = cat.fit_transform(adata)
+    cat.save_results(out_dir, 'starCAT')
 
-    return processed_data
+    with tarfile.open(out_file, "w:gz") as tar:
+        tar.add(out_dir, arcname = 'starCAT_output')
 
-'''
-@bp.route('/',methods=('GET', 'POST'))
-#@manage_cookie_policy
-def niceurlsspawn():
-    mc = set_menu("starcat")
-    page_title = "This is a title that will end up in the page url"
-    page_title_for_id = "This is a title that will end up in the page url"
-    return render_template('starcat/niceurlsspawn.html', mc=mc, 
-        page_title=page_title, page_title_for_id=page_title_for_id)
 
-@bp.route('/<slug>',methods=('GET', 'POST'))
-#@manage_cookie_policy
-def niceurl(slug=''):
-    mc = set_menu("starcat")
-    page_title = "This is a title that will end up in the page url"
-    return render_template('starcat/pageniceurl.html', mc=mc, page_title=page_title)
-
-@bp.route('/<slug>/<int:id>',methods=('GET', 'POST'))
-#@manage_cookie_policy
-def niceurlid(slug='', id=0):
-    mc = set_menu("starcat")
-    page_title_for_id = "This is a title that will end up in the page url"
-    return render_template('starcat/pageniceurlid.html', mc=mc, page_title_for_id=page_title_for_id)
-'''
