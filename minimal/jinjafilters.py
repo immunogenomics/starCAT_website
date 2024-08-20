81a1caffe4cd7ffbd5bcaab3250c2e0d12680589
michelle-curtis <mcurtis459@gmail.com>
2024-08-20 13:01:52 -0400

    add upload error messages

--- minimal/jinjafilters.py
+++ minimal/jinjafilters.py
@@ -20,8 +20,11 @@ def displayError(errNum):
 msgDict = { 
     "Msg1": "<p>This is a <b>nice</b> message, the first of the list</p>",
     "Msg2": "<p>This is an even <b>nicer</b> message.</p>",
-    'select_ref_message' : '<p>Please first submit a reference catalog selection and upload again.</p>',
+    # 'select_ref_message' : '<p>Please first submit a reference catalog selection and upload again.</p>',
     'Login Unsuccessful' : '<p>Login Unsuccessful. Please check email and password and make sure you have already registered.</p>',
+    'File Type Error' : '<p>File Type Error. Please upload an ".h5ad" type file.</p>',
+    'File Size Error' : '<p>File Size Error. Please upload a smaller file (<2 GB) or refer to our <a href="https://github.com/immunogenomics/starCAT" target="_blank">'
+            'GitHub repository</a>  process larger datasets.</p>',
 
     # 'processing_message' : '<p>Your data is processing.</p>',
     
