01f6c7d84d9f7cc48ffe64fbf9d6c2653ae66169
Dylan Kotliar <dylkot@gmail.com>
2024-08-20 11:25:38 -0400

    fix messages when registering and logging in

--- minimal/jinjafilters.py
+++ minimal/jinjafilters.py
@@ -21,7 +21,8 @@ msgDict = {
     "Msg1": "<p>This is a <b>nice</b> message, the first of the list</p>",
     "Msg2": "<p>This is an even <b>nicer</b> message.</p>",
     'select_ref_message' : '<p>Please first submit a reference catalog selection and upload again.</p>',
-    'Login Unsuccessful' : '<p>Login Unsuccessful. Please check email and password and make sure you have already registered.</p>',
+    'Login Unsuccessful. Please check email and password and make sure you have already registered.' : '<p>Login Unsuccessful. Please check email and password and make sure you have already registered.</p>',
+    'Your account has been created! You can now log in.' : '<p>Your account has been created! You can now log in.</p>'
 
     # 'processing_message' : '<p>Your data is processing.</p>',
     
