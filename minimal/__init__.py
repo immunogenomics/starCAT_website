4c2227fde912d35a52277d7cfca047a1814cd7a0
Dylan Kotliar <dylkot@gmail.com>
2024-08-20 01:20:08 -0400

    started user registration/login

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -1,15 +1,23 @@
 import os
 from flask import Flask
+from flask_login import LoginManager
 from .jinjafilters import *
 from .errorhandlers import *
 from .config import Config
 from .user_model import db
+
+
+login_manager = LoginManager()
 
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
     db.init_app(app)
+
+    login_manager.init_app(app)
+    #login_manager.login_view = 'bp.login'
+    #login_manager.login_message_category = 'info'
 
     from . import bl_home
     app.register_blueprint(bl_home.bp)
@@ -20,7 +28,8 @@ def create_app():
     from . import bl_starcat
     app.register_blueprint(bl_starcat.bp)
 
-    #Add other blueprints if needed
+    from . import bl_login
+    app.register_blueprint(bl_login.bp)
 
     from . import auth
     app.register_blueprint(auth.bp)
@@ -35,3 +44,8 @@ def create_app():
     app.jinja_env.filters['displayMessage'] = displayMessage
 
     return app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
---
+++ minimal/user_model.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
