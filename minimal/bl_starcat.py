d04c2b1eca52bb2f87a82b5de13d4fb1a024a48d
Dylan Kotliar <dylkot@gmail.com>
2024-08-26 22:49:32 -0400

    split login and run starcat

--- minimal/bl_starcat.py
+++ minimal/bl_starcat.py
@@ -18,61 +18,50 @@ from minimal import reference_names
 bcrypt = Bcrypt()
 bp = Blueprint('bl_starcat', __name__, url_prefix='/starcat')
 
-@bp.route('/run-starcat', methods=('GET', 'POST'))
-def runstarcat():
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
     mc = set_menu("starcat")
 
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        
-        # Authenticate user
         user = User.query.filter_by(email=email).first()
-        if user and bcrypt.check_password_hash(user.password, password) and user.is_email_verified:
-            session['selected_ref'] = request.form.get('ref')
-            file = request.files['file']
-            
-            if file:
-                if os.path.splitext(file.filename)[-1] != '.h5ad':
-                    flash("File Type Error", 'danger')
-                else:
-                    print('hello')
-                    id = ''.join(random.choice(string.ascii_letters) for _ in range(15))
-                    os.makedirs(os.path.join(current_app.config['UPLOAD_FOLDER'], id), exist_ok=True)
-                    file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], id, file.filename)
-                    file.save(file_path)
-
-                    if session.get('selected_ref'):
-                        process_data(file_path, id)
-                        out_file = os.path.join(current_app.config['UPLOAD_FOLDER'], id, 'starCAT_output.tar.gz')
-                        return send_file(os.path.join(os.getcwd(), out_file), as_attachment=True,
-                                         download_name='starCAT_output.tar.gz')
+        if not user:
+            flash('Email not recognized. Please register', 'danger')
+            return redirect(url_for('bl_starcat.login'))
+        elif not user.is_email_verified:
+            flash('Email not verified. Please check your email to confirm registration', 'danger')
+            return redirect(url_for('bl_starcat.login'))
+        elif not bcrypt.check_password_hash(user.password, password):
+            flash('Incorrect password. Please double check or reset if necessary', 'danger')
+            return redirect(url_for('bl_starcat.login'))
         else:
-            flash('Invalid email or password. Please check they are entered correctly and make sure you have already registered\
-                and verified your email', 'danger')
+            session['verified'] = True
             return redirect(url_for('bl_starcat.runstarcat'))
 
-    return render_template('starcat/starcatpage.html', mc=mc, references=reference_names, 
-                           selected_ref=session.get('selected_ref'))
+    if not session.get('verified'):
+        return render_template('starcat/login.html', mc=mc, references=reference_names)
+    else:
+        return render_template('starcat/starcatpage.html', mc=mc, references=reference_names)
 
-@bp.errorhandler(RequestEntityTooLarge)
-def file_size_error(error):
-    flash('File Size Error', 'danger')
-    return redirect(url_for('bl_starcat.runstarcat'))
 
-def process_data(file_path, id):
-    # Run starCAT
-    out_dir = os.path.join(current_app.config['UPLOAD_FOLDER'], id, 'starCAT_output')
-    out_file = os.path.join(current_app.config['UPLOAD_FOLDER'], id, 'starCAT_output.tar.gz')
-    os.makedirs(out_dir, exist_ok=True)
+@bp.route('/run-starcat', methods=('GET', 'POST'))
+def runstarcat():
+    mc = set_menu("starcat")
+    if not session.get('verified'):
+        flash('Please login', 'danger')
+        return redirect(url_for('bl_starcat.login'))
 
-    cat = starCAT(reference=session.get('selected_ref'), cachedir=current_app.config['UPLOAD_FOLDER'])
-    adata = cat.load_counts(file_path)
-    usage, scores = cat.fit_transform(adata)
-    cat.save_results(out_dir, 'starCAT')
+    return render_template('starcat/starcatpage.html', mc=mc, references=reference_names)
 
-    with tarfile.open(out_file, "w:gz") as tar:
-        tar.add(out_dir, arcname='starCAT_output')
+
+@bp.route('/logout')
+def logout():
+    # Clear the session to log the user out
+    session.pop('verified', None)
+    # Redirect the user back to the login page
+    return redirect(url_for('bl_starcat.login'))
+
 
 @bp.route("/reset_password", methods=['GET', 'POST'])
 def reset_request():
